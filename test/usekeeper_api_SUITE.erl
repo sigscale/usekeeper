c0156ee8fc649ff4c1a401eceeff2d31162f6214
Vance Shipley <vances@sigscale.org>
2019-11-09 17:09:24 +0800

    test cases for user api

--- test/usekeeper_api_SUITE.erl
+++ test/usekeeper_api_SUITE.erl
@@ -29,6 +29,7 @@
 
 -include("usage.hrl").
 -include_lib("common_test/include/ct.hrl").
+-include_lib("inets/include/mod_auth.hrl").
 
 %%---------------------------------------------------------------------
 %%  Test server callback functions
@@ -76,14 +77,99 @@ sequences() ->
 %% Returns a list of all test cases in this test suite.
 %%
 all() ->
-	[].
+	[add_user, get_user, list_users, delete_user].
 
 %%---------------------------------------------------------------------
 %%  Test cases
 %%---------------------------------------------------------------------
 
+add_user() ->
+	[{userdata, [{doc, "Create a new HTTP user"}]}].
+
+add_user(_Config) ->
+	User = random_string(),
+	Password = random_string(),
+	{ok, {TS, N} = LastModified} = usekeeper:add_user(User, Password),
+	true = is_integer(TS),
+	true = is_integer(N),
+	{Port, Address, Dir, _} = get_params(),
+	{ok, #httpd_user{username = User, password = Password,
+			user_data = UserData}} = mod_auth:get_user(User, Address, Port, Dir),
+	{_, LastModified} = lists:keyfind(last_modified, 1, UserData).
+
+get_user() ->
+	[{userdata, [{doc, "Look up an HTTP user"}]}].
+
+get_user(_Config) ->
+	User = random_string(),
+	Password = random_string(),
+	{ok, {TS, N} = LastModified} = usekeeper:add_user(User, Password),
+	true = is_integer(TS),
+	true = is_integer(N),
+	{ok, #httpd_user{username = User, password = Password,
+			user_data = UserData}} = usekeeper:get_user(User),
+	{_, LastModified} = lists:keyfind(last_modified, 1, UserData).
+
+list_users() ->
+	[{userdata, [{doc, "List all HTTP users"}]}].
+
+list_users(_Config) ->
+	User1 = random_string(),
+	{ok, _} = usekeeper:add_user(User1, random_string()),
+	User2 = random_string(),
+	{ok, _} = usekeeper:add_user(User2, random_string()),
+	User3 = random_string(),
+	{ok, _} = usekeeper:add_user(User3, random_string()),
+	{ok, Users} =  usekeeper:list_users(),
+	true = lists:all(fun erlang:is_list/1, Users),
+	true = lists:member(User1, Users),
+	true = lists:member(User2, Users),
+	true = lists:member(User3, Users).
+
+delete_user() ->
+	[{userdata, [{doc, "Remove an HTTP user"}]}].
+
+delete_user(_Config) ->
+	User = random_string(),
+	Password = random_string(),
+	{ok, _} = usekeeper:add_user(User, Password),
+	{ok, _} = usekeeper:get_user(User),
+	ok = usekeeper:delete_user(User),
+	{error, no_such_user} = usekeeper:get_user(User).
 
 %%---------------------------------------------------------------------
 %%  Internal functions
 %%---------------------------------------------------------------------
+
+-spec random_string() -> String :: string().
+%% @doc Generate a random string.
+%% @private
+random_string() ->
+	random_string(rand:uniform(16) + 4).
+
+-spec random_string(Length :: pos_integer()) -> String :: string().
+%% @doc Generate a random string.
+%% @private
+random_string(Length) ->
+	random_string(Length, []).
+%% @hidden
+random_string(0, Acc) ->
+	Acc;
+random_string(N, Acc) ->
+	random_string(N - 1, [rand:uniform(95) + 31 | Acc]).
+
+%% @hidden
+get_params() ->
+	{_, _, Info} = lists:keyfind(httpd, 1, inets:services_info()),
+	{_, Port} = lists:keyfind(port, 1, Info),
+	{_, Address} = lists:keyfind(bind_address, 1, Info),
+	{ok, EnvObj} = application:get_env(inets, services),
+	{httpd, HttpdObj} = lists:keyfind(httpd, 1, EnvObj),
+	{directory, {Directory, AuthObj}} = lists:keyfind(directory, 1, HttpdObj),
+	case lists:keyfind(require_group, 1, AuthObj) of
+		{require_group, [Group | _T]} ->
+			{Port, Address, Directory, Group};
+		false ->
+			exit(not_found)
+	end.
 
