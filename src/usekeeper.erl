402b1a5ec298b58d76c5314df0bc53a3e58fd58f
Vance Shipley <vances@sigscale.org>
2019-11-09 16:25:07 +0800

    http user api

--- src/usekeeper.erl
+++ src/usekeeper.erl
@@ -22,19 +22,166 @@
 -copyright('Copyright (c) 2019 SigScale Global Inc.').
 
 %% export the usekeeper public API
--export([]).
+-export([add_user/2, list_users/0, get_user/1, delete_user/1]).
 
 %% export the usekeeper private API
 -export([]).
 
 -include("usage.hrl").
+-include_lib("inets/include/mod_auth.hrl").
 
 %%----------------------------------------------------------------------
 %%  The usekeeper public API
 %%----------------------------------------------------------------------
 
+-spec add_user(Username, Password) -> Result
+	when
+		Username :: string(),
+		Password :: string(),
+		Result :: {ok, LastModified} | {error, Reason},
+		LastModified :: {integer(), integer()},
+		Reason :: user_exists | term().
+%% @doc Add an HTTP user.
+%%
+%% 	HTTP Basic authentication (RFC7617) is required with
+%% 	`Username' and  `Password' used to construct the
+%% 	`Authorization' header in requests.
+%%
+add_user(Username, Password) when is_list(Username),
+		is_list(Password) ->
+	add_user1(Username, Password, get_params()).
+%% @hidden
+add_user1(Username, Password, {Port, Address, Dir, Group}) ->
+	add_user2(Username, Password,
+			Address, Port, Dir, Group, usekeeper:get_user(Username));
+add_user1(_, _, {error, Reason}) ->
+	{error, Reason}.
+%% @hidden
+add_user2(Username, Password,
+		Address, Port, Dir, Group, {error, no_such_user}) ->
+	LM = {erlang:system_time(millisecond), erlang:unique_integer([positive])},
+	NewUserData = [{last_modified, LM}],
+	add_user3(Username, Address, Port, Dir, Group, LM,
+			mod_auth:add_user(Username, Password, NewUserData, Address, Port, Dir));
+add_user2(_, _, _, _, _, _, {error, Reason}) ->
+	{error, Reason};
+add_user2(_, _, _, _, _, _, {ok, _}) ->
+	{error, user_exists}.
+%% @hidden
+add_user3(Username, Address, Port, Dir, Group, LM, true) ->
+	add_user4(LM, mod_auth:add_group_member(Group, Username, Address, Port, Dir));
+add_user3(_, _, _, _, _, _, {error, Reason}) ->
+	{error, Reason}.
+%% @hidden
+add_user4(LM, true) ->
+	{ok, LM};
+add_user4(_, {error, Reason}) ->
+	{error, Reason}.
+
+-spec list_users() -> Result
+	when
+		Result :: {ok, Users} | {error, Reason},
+		Users :: [Username],
+		Username :: string(),
+		Reason :: term().
+%% @doc List HTTP users.
+%% @equiv  mod_auth:list_users(Address, Port, Dir)
+list_users() ->
+	list_users1(get_params()).
+%% @hidden
+list_users1({Port, Address, Dir, _}) ->
+	mod_auth:list_users(Address, Port, Dir);
+list_users1({error, Reason}) ->
+	{error, Reason}.
+
+-spec get_user(Username) -> Result
+	when
+		Username :: string(),
+		Result :: {ok, User} | {error, Reason},
+		User :: #httpd_user{},
+		Reason :: term().
+%% @doc Get an HTTP user record.
+%% @equiv mod_auth:get_user(Username, Address, Port, Dir)
+get_user(Username) ->
+	get_user(Username, get_params()).
+%% @hidden
+get_user(Username, {Port, Address, Dir, _}) ->
+	mod_auth:get_user(Username, Address, Port, Dir);
+get_user(_, {error, Reason}) ->
+	{error, Reason}.
+
+-spec delete_user(Username) -> Result
+	when
+		Username :: string(),
+		Result :: ok | {error, Reason},
+		Reason :: term().
+%% @doc Delete an existing HTTP user.
+delete_user(Username) ->
+	delete_user1(Username, get_params()).
+%% @hidden
+delete_user1(Username, {Port, Address, Dir, GroupName}) ->
+	delete_user2(GroupName, Username, Address, Port, Dir,
+			mod_auth:delete_user(Username, Address, Port, Dir));
+delete_user1(_, {error, Reason}) ->
+	{error, Reason}.
+%% @hidden
+delete_user2(GroupName, Username, Address, Port, Dir, true) ->
+	delete_user3(mod_auth:delete_group_member(GroupName,
+			Username, Address, Port, Dir));
+delete_user2(_, _, _, _, _, {error, Reason}) ->
+	{error, Reason}.
+%% @hidden
+delete_user3(true) ->
+	ok;
+delete_user3({error, Reason}) ->
+	{error, Reason}.
+
 
 %%----------------------------------------------------------------------
 %%  internal functions
 %%----------------------------------------------------------------------
+
+-spec get_params() -> Result
+	when
+		Result :: {Port :: integer(), Address :: string(),
+		Directory :: string(), Group :: string()}
+				| {error, Reason :: term()}.
+%% @doc Returns configurations details for currently running
+%% 	{@link //inets. httpd} service.
+%% @hidden
+get_params() ->
+	get_params(inets:services_info()).
+%% @hidden
+get_params({error, Reason}) ->
+	{error, Reason};
+get_params(ServicesInfo) ->
+	get_params1(lists:keyfind(httpd, 1, ServicesInfo)).
+%% @hidden
+get_params1({httpd, _, HttpdInfo}) ->
+	{_, Address} = lists:keyfind(bind_address, 1, HttpdInfo),
+	{_, Port} = lists:keyfind(port, 1, HttpdInfo),
+	get_params2(Address, Port, application:get_env(inets, services));
+get_params1(false) ->
+	{error, httpd_not_started}.
+%% @hidden
+get_params2(Address, Port, {ok, Services}) ->
+	get_params3(Address, Port, lists:keyfind(httpd, 1, Services));
+get_params2(_, _, undefined) ->
+	{error, inet_services_undefined}.
+%% @hidden
+get_params3(Address, Port, {httpd, Httpd}) ->
+	get_params4(Address, Port, lists:keyfind(directory, 1, Httpd));
+get_params3(_, _, false) ->
+	{error, httpd_service_undefined}.
+%% @hidden
+get_params4(Address, Port, {directory, {Directory, Auth}}) ->
+	get_params5(Address, Port, Directory,
+			lists:keyfind(require_group, 1, Auth));
+get_params4(_, _, false) ->
+	{error, httpd_directory_undefined}.
+%% @hidden
+get_params5(Address, Port, Directory, {require_group, [Group | _]}) ->
+	{Port, Address, Directory, Group};
+get_params5(_, _, _, false) ->
+	{error, httpd_group_undefined}.
 
