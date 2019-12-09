db8b73766e3a7f9dd081bec197790011ea07d291
kusal fernando <kusal@sigscale.com>
2019-12-09 12:32:14 +0000

    codec for usage

--- src/usekeeper_rest_res_usage.erl
+++ src/usekeeper_rest_res_usage.erl
@@ -51,12 +51,13 @@ content_types_provided() ->
 		RequestBody :: list(),
 		Result :: {ok, Headers :: [tuple()], Body :: iolist()}
 			| {error, ErrorCode :: integer()}.
-%% @doc Handle `POST' request on `UsageSpecification' collection.
+%% @doc Handle `POST' request on `Usage' collection.
 post_usage(RequestBody) ->
 	try
 		{ok, Usage} = zj:decode(RequestBody),
-		case usekeeper:add_usage(Usage) of
-			{ok, UsageLog} ->
+		case usekeeper:add_usage(usage(Usage)) of
+			{ok, {TS, N, U}} ->
+				UsageLog = {TS, N, usage(U)},
 				Body = zj:encode(UsageLog),
 				Headers = [{content_type, "application/json"}],
 				{ok, Headers, Body};
@@ -67,3 +68,57 @@ post_usage(RequestBody) ->
 		_:_Reason1 ->
 			{error, 400}
 	end.
+
+-spec usage(Usage) -> Usage
+	when
+		Usage :: map().
+%% @doc CODEC for `Usage'.
+usage(#{} = Usage) ->
+	usage(maps:keys(Usage), Usage).
+%% @hidden
+usage(["usageCharacteristic" | T],
+		#{"usageCharacteristic" := UsageChar} = U) when is_map(UsageChar) ->
+	I = maps:iterator(UsageChar),
+	UsageCharList = map_to_list(maps:next(I), []),
+	usage(T, U#{"usageCharacteristic" => UsageCharList});
+usage(["usageCharacteristic" | T],
+		#{"usageCharacteristic" := UsageChar} = U) when is_list(UsageChar) ->
+	UsageCharMap = list_to_map(UsageChar, 1, #{}),
+	usage(T, U#{"usageCharacteristic" => UsageCharMap});
+usage(["ratedProductUsage" | T],
+		#{"ratedProductUsage" := Rated} = U) when is_map(Rated) ->
+	I = maps:iterator(Rated),
+	RatedList = map_to_list(maps:next(I), []),
+	usage(T, U#{"ratedProductUsage" => RatedList});
+usage(["ratedProductUsage" | T],
+		#{"ratedProductUsage" := Rated} = U) when is_list(Rated) ->
+	RatedMap = list_to_map(Rated, 1, #{}),
+	usage(T, U#{"ratedProductUsage" => RatedMap});
+usage(["relatedParty" | T],
+		#{"relatedParty" := Related} = U) when is_map(Related) ->
+	I = maps:iterator(Related),
+	RelatedList = map_to_list(maps:next(I), []),
+	usage(T, U#{"relatedParty" => RelatedList});
+usage(["relatedParty" | T],
+		#{"relatedParty" := Related} = U) when is_list(Related) ->
+	RelatedMap = list_to_map(Related, 1, #{}),
+	usage(T, U#{"relatedParty" => RelatedMap});
+usage([_H | T], Usage) ->
+	usage(T, Usage);
+usage([], Usage) ->
+	Usage.
+
+%%----------------------------------------------------------------------
+%%  internal functions
+%%----------------------------------------------------------------------
+
+list_to_map([H | T], N, Acc) ->
+	Key = integer_to_list(N),
+	list_to_map(T, N + 1, Acc#{Key => H});
+list_to_map([], _N, Acc) ->
+	Acc.
+
+map_to_list({_Key, Value, I}, Acc) ->
+	map_to_list(maps:next(I), [Value | Acc]);
+map_to_list(none, Acc) ->
+	Acc.
