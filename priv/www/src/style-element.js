632c47ab975118a36f524cafabf05cb0638f8592
Vance Shipley <vances@sigscale.org>
2019-12-16 17:58:12 +0800

    remove padding from progress bar; fixes horizontal scrollbar in dialog

--- priv/www/src/style-element.js
+++ priv/www/src/style-element.js
@@ -49,6 +49,7 @@ styleElement.innerHTML = `<template>
 				display: block;
 				width: 100%;
 				margin: 0px;
+				padding: 0px;
 				--paper-progress-active-color: var(--paper-lime-a700);
 				--paper-progress-container-color: transparent;
 			}
