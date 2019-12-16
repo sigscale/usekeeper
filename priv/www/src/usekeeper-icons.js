fa61d7209aeab946de77a5a50c3b22bc37842012
Vance Shipley <vances@sigscale.org>
2019-12-16 21:09:27 +0800

    rename

--- priv/www/src/usekeeper-icons.js
+++ priv/www/src/usekeeper-icons.js
@@ -31,7 +31,7 @@ $_documentContainer.innerHTML = `<iron-iconset-svg name="my-icons" size="24">
 			<g id="users">
 				<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"></path>
 			</g>
-			<g id="usageIcon">
+			<g id="usage">
 				<path d="M3 13h2v-2H3v2zm0 4h2v-2H3v2zm0-8h2V7H3v2zm4 4h14v-2H7v2zm0 4h14v-2H7v2zM7 7v2h14V7H7z"></path>
 			</g>
 			<g id="spec">
