b67be411cea383f321c4034beebc5cb93f1b4e12
krishmanl <krishmanl@sigscale.com>
2019-12-17 06:59:23 +0000

    Revert "add paper fab button"
    This reverts commit 5d9f85dd46f75bd642f67b2959afb422dbdaf714.

--- priv/www/src/usekeeper-usage-list.js
+++ priv/www/src/usekeeper-usage-list.js
@@ -11,7 +11,6 @@
 import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import '@polymer/iron-ajax/iron-ajax.js';
 import '@vaadin/vaadin-grid/vaadin-grid.js';
-import '@polymer/paper-fab/paper-fab.js';
 import './style-element.js';
 
 class usageList extends PolymerElement {
@@ -59,12 +58,6 @@ class usageList extends PolymerElement {
 					<template>[[item.type]]</template>
 				</vaadin-grid-column>
 			</vaadin-grid>
-			<div class="add-button">
-				<paper-fab
-						icon="my-icons:add"
-						on-tap="_usageAddOpenModal">
-				</paper-fab>
-			</div>
 			<iron-ajax
 				id="getUsageAjax"
 				url="usageManagement/v4/usage"
